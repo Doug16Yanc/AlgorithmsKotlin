77d2c90f7b42ca0263223a8757afdd9510a96435
Douglas Holanda <douglasholanda3195@gmail.com>
2023-12-20 08:35:43 -0300

    Question 12 correction

--- src/main/kotlin/List1/question12.kt
+++ src/main/kotlin/List1/question12.kt
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
 
     var horas = (segundos/3600)
     var minutos = (horas*60)%60
-    var segundosRes = segundos%3600
+    var segundosRes = segundos - (horas.toInt()*3600 + minutos.toInt()*60)
 
-    println("Temos ${horas.toInt()} hora(s), ${minutos.toInt()} minuto(s), ${segundosRes.toInt()} segundo(s).")
+    println("Temos ${horas.toInt()} hora(s), ${minutos.toInt()} minuto(s), ${segundosRes} segundo(s).")
 }
